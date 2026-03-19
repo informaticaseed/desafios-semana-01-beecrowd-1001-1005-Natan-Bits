00d474eadc761e5a57e3413de556ce063446ffb9
Natan Nagashima <natan.nagashima.dev@gmail.com>
2026-03-19 09:12:02 -0300

    Update desafio_1001.py

--- desafio_1001.py
+++ desafio_1001.py
@@ -1,8 +1,4 @@
-
-
-# Link do problema: https://judge.beecrowd.com/pt/problems/view/1001
-
-# Escreva sua solução abaixo
-A,B=int(input()), int(input())
+A=int(input())
+B=int(input()
 X=A+B
 print(X)
